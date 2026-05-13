feae9fa50312b107e6fcced13b23828816750c73
Pete Fluriach <faithdrivencreator@gmail.com>
2026-05-13 09:51:54 -0400

    debug: ignore TS/ESLint build errors to isolate failure

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
