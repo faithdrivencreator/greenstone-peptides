b08989ac7d35439bd0442d232cd2441ab98c0234
Pete Fluriach <faithdrivencreator@gmail.com>
2026-05-13 19:50:49 -0400

    chore: remove Sanity — sanity/schemas/index.ts

--- sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import product from './product';
-import category from './category';
-import blogPost from './blogPost';
-import author from './author';
-import siteSettings from './siteSettings';
-
-export const schemaTypes = [product, category, blogPost, author, siteSettings];
